43294dc30abe5069d3f21d259ba4f0ed19c72fe9
melondulzon <melondulzon@gmail.com>
2020-03-18 02:25:46 +0100

    deploy
    afegida configuració flaskenv, canvis a run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ from app import app
 """
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0,0,0,0'), port=os.getenv('PORT', '5000'), debug=False)
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
 
